47cdbca7dd721487a47ddbea1a2f400d20ce67ea
Igor Lucio Vieira <lucioigf2@gmail.com>
2022-02-06 04:00:48 -0300

    routes; adjustments on navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ArtworksComponent } from './artworks/artworks.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'artworks', component:  ArtworksComponent },
+	{ path: 'artists', component: ArtistsComponent },
+	{ path: 'myorders', component: MyOrdersComponent },
+	{ path: 'singin', component: SignInComponent },
+	{ path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ButtonComponent } from './button/button.component';
 import { SearchInputComponent } from './search-input/search-input.component';
+import { HomeComponent } from './home/home.component';
+import { ArtworksComponent } from './artworks/artworks.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-	declarations: [AppComponent, NavComponent, ButtonComponent, SearchInputComponent],
+	declarations: [AppComponent, NavComponent, ButtonComponent, SearchInputComponent, HomeComponent, ArtworksComponent, ArtistsComponent, MyOrdersComponent, SignInComponent, RegisterComponent],
 	imports: [BrowserModule, AppRoutingModule, IconsModule],
 	providers: [],
 	bootstrap: [AppComponent]
